4eafdbeb5db328012093dbd608d722304fc215df
碳酸氢钠 一氧化二氢溶液 <huz193py@outlook.com>
2025-03-21 12:00:14 +0800

    main.js添加代码

---
+++ assets/JS/mian.js
@@ -0,0 +1,4 @@
+const text = document.getElementById("text");
+const input = document.getElementById("input");
+
+const specialKeys = ["CapsLock", "Shift", "Control", "Alt", "Meta", "Tab", "Escape", "Enter", "Backspace","Arrow"];
